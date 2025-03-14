9bdc6184d184d0d512cde49258bfed27086399d3
Pranav Gupta <146617022+Pranav-7302@users.noreply.github.com>
2025-03-14 15:22:30 +0530

    Question Solved

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Complete the js code
-function Car(make, model) {}
+class Car{
+    constructor(make,model){
+        this.make = make
+        this.model = model
+    }
+    getMakeModel(){
+        return `${this.make} ${this.model}`
+    }
 
-function SportsCar(make, model, topSpeed) {}
-
+}
+class SportsCar extends Car{
+    constructor(make,model,topSpeed){
+        super(make,model)
+        this.topSpeed = topSpeed
+    }
+    getTopSpeed(){
+        return this.topSpeed;
+    }
+}
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
